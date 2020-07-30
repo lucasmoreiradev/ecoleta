3f5dc2a3b58a3a795019ade3a9b97360a73a6029
Lucas Moreira <eskerlucas1@gmail.com>
2020-07-30 18:31:29 -0300

    feat: add celebration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import { errors } from 'celebrate'
 import cors from 'cors'
 import express from 'express'
 import path from 'path'
@@ -11,5 +12,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333)
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi } from 'celebrate'
 import express from 'express'
 import multer from 'multer'
 
@@ -10,7 +11,29 @@ const routes = express.Router()
 const upload = multer(multerConfig)
 
 routes.get('/items', ItemsController.index)
-routes.post('/points', upload.single('image'), PointsController.create)
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required()
+      })
+    },
+    {
+      abortEarly: false
+    }
+  ),
+  PointsController.create
+)
 routes.get('/points/', PointsController.index)
 routes.get('/points/:id', PointsController.show)
 
